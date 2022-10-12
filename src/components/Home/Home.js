4fd7e1577bfaaf81325c540c5f558fee194dabd6
Arifjawad18 <arif.jawad18@gmail.com>
2022-10-12 15:57:37 +0600

    nav add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Product from '../Product/Product';
-
+import './Home.css'
 const Home = () => {
     const quiz = useLoaderData();
 
     return (
         <div>
-           {
-            quiz.map(quiz => <Product
-                key={quiz.id}
-                quiz={quiz}
-            ></Product>)
-           }
+              <div>
+        <h1>Start Answering</h1>
+        <p>Choose your favorite topic</p>
         </div>
+            <div className="home-container">
+                <div className="product-container">
+                {
+                     quiz.map(quiz =><Product
+                     key={quiz.id}
+                    quiz={quiz}
+                    ></Product>)
+           }
+                </div>
+            </div>
+            </div>
+
+
+
+         
+        
     );
 };
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import './Product.css'
 
 const Product = ({quiz}) => {
-    const {picture} = quiz;
+    const {picture, name, total} = quiz;
     return (
-      
-       <div>
-        <h2 className='text-center'>Select Your 4 Quiz</h2>
- <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={picture} />
-      <Card.Body>
-        <Card.Title>React</Card.Title>
-        <Card.Text>
-       
-        </Card.Text>
-        <Button variant="primary">Go Quiz</Button>
-      </Card.Body>
-    </Card> 
-    
-
-
-
+       <div className='product'>
+        <img src={picture} alt="" />
+        <h2>{name}</h2>
+        <h4>Total: {total}</h4>
+        <button className='button'>Click</button>
        </div>
     )
         }
